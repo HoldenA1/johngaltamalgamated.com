49c867fc49d1475e4575a3f577315f84e742711d
Holden Adamec <holden.adamec@gmail.com>
2020-02-19 16:57:31 -0800

    Got responsive navbar working

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ if (w >= 1200) {
   var att = document.createAttribute("auto-rotate");
   model.setAttributeNode(att);
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function toggleNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
